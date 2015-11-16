26a88432d290b73818549507451600060ca6ace9
Shingo Kitagawa <shingogo@hotmail.co.jp>
2015-11-16 10:50:35 +0900

    add class in rospy run file

--- baxter_vision_example/scripts/tf_listener.py
+++ baxter_vision_example/scripts/tf_listener.py
@@ -5,22 +5,30 @@ roslib.load_manifest('baxter_vision_example')
 import rospy
 import tf
 
-if __name__ =='__main__':
-    rospy.init_node('baxter_tf_listener')
-    trans = []
-    rot = []
-
-    listener = tf.TransformListener()
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        try:
-            (trans,rot) = listener.lookupTransform('right_gripper','base',rospy.Time(0))
-               
-        except (tf.LookupException,tf.ConnectivityException,tf.ExtrapolationException):
-            pass
+class tf_listener:
+    def __init__ (self):
+         rospy.init_node('baxter_tf_listener')
         
-        if (trans and rot):
-            print 'trans: %f %f %f ' % (trans[0], trans[1], trans[2])
-            print 'rot  : %f %f %f %f ' % (rot[0], rot[1], rot[2],rot[3])
+    def listener(self,from_tf, to_tf):
+        trans = []
+        rot = []
 
-        rate.sleep()
+        listener = tf.TransformListener()
+        rate = rospy.Rate(10.0)
+        while not rospy.is_shutdown():
+            try:
+                (trans,rot) = listener.lookupTransform(from_tf,to_tf,rospy.Time(0))
+               
+            except (tf.LookupException,tf.ConnectivityException,tf.ExtrapolationException):
+                pass
+        
+            if (trans and rot):
+               print 'trans: %f %f %f ' % (trans[0], trans[1], trans[2])
+               print 'rot  : %f %f %f %f ' % (rot[0], rot[1], rot[2],rot[3])
+            rate.sleep()
+
+if __name__ =='__main__':
+    tfl = tf_listener()
+    tfl.listener('base','right_gripper')
+
+
